fe0f759ea8831d221df520250838792f2cac464d
Dheeraj Ponnoju <dheerajponnoju@gmail.com>
2021-02-09 13:12:09 +0000

    18:14 09-02-2021

---
+++ abc.py
@@ -0,0 +1,50 @@
+a = 1
+print(a)
+print(type(a))
+a = 2.5
+print(a)
+print(type(a))
+a = "Hello, World!"
+print(a)
+print(type(a))
+b = "python"
+print(a+b)
+c = 2
+d = 3
+e = 4.5
+f = "Hello"
+print(c+d)
+print(c+e)
+print(str(c)+" "+f)
+print(1+7)
+tf1 = True
+tf2 = False
+print(tf1)
+print(tf2)
+#Arithmetic Operations
+"""Hello, World! I am PC , 
+Happy New Year
+How are you"""
+'''Ji
+KI
+Lo'''
+'''Addition
+Subtraction
+Multiplication
+Division - Quotient, Division, Remainder 
+Exponentiation '''
+
+c = 4
+d = 2.5
+print(c-d)
+print(int(c-d))
+print(float(12))
+print(d-c)
+print(c*d)
+
+e = 2
+print(c/d)
+print(c%e)
+
+print(3//2)
+print(2**3)
